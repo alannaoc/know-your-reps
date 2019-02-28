4bc532afcfd2048321d7a7e60036148225bb59bd
Alanna <oconnor.alannae@gmail.com>
2019-02-28 11:32:46 -0500

    second commit, with input errors handled

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     super();
     this.state = {
       userReps: [],
+      userMayor: [],
       userPostalCode: '',
       query: ''
     }
@@ -26,16 +27,34 @@ class App extends Component {
         }}).then((res) => {
             const repsResult = res.data.representatives_centroid;
             console.log(repsResult);
+
+
+            let uniqueRepsResult = repsResult.map(reps => {
+              return reps.elected_office
+            })
+            
+            // [... new Set(repsResult)]
+            console.log(uniqueRepsResult);
             this.setState({
             userReps: repsResult
           })
         });
     }
 
+ //handle change converts the user's input from lower case to uppercase and removes any white spaces. 
     handleChange = (e) => {
-      this.setState({
+     e.target.value = e.target.value.toUpperCase();
+      if (e.target.value.indexOf(' ') >= 0) {
+        this.setState({
+          userPostalCode: e.target.value.replace(/\s/g, '')
+        })
+        console.log(e.target.value.replace(/\s/g, ''));
+      } else {
+        this.setState({
         userPostalCode: e.target.value
-      })
+        })
+      }
+      
     }
 //     componentDidMount(){
 //       // axios({
@@ -49,17 +68,34 @@ class App extends Component {
       
 //     }
 
-
   render() {
     return (
       <div className="App">
-        <h1>Engaged Citizens</h1>
-        <h2>Find out who your local represenatives are and how to contact them.</h2>
+        <h1>Know your government</h1>
+        <h2>Find your local representatives.</h2>
         <form action="submit" onSubmit={this.handleSubmit}>
           <label htmlFor="postCode">Enter your postal code:</label>
-          <input type="text" placeholder="enter your postal code (i.e A1A1A1" onChange={this.handleChange} value={this.state.query}/>
+          <input type="text" placeholder="enter your postal code (i.e A1A1A1" onChange={this.handleChange}/>
           <button type="submit">Find out!</button>
         </form>
+        {/* {
+          this.state.userReps.map(rep => {
+            return(
+              <div key={rep.i}>
+                <Reps 
+                  title={rep.name}
+                  district={rep.district_name}
+                  office={rep.elected_office}
+                  party={rep.party_name}
+                  leg={rep.representative_set_name}
+                  website={rep.url}
+                  email={rep.email}
+                  phone={rep.offices[0].type, rep.offices[0].tel}
+                />
+              </div>
+            )
+          })
+        } */}
       </div>
     );
   }
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './Header.css';
+
+class App extends Component {
+  render() {
+    return(
+      <div className='Header'>
+        <header>
+          <h1>Know you government</h1>
+          <h2>Find your local representatives</h2>
+        </header>
+      </div>
+    )
+  }
+}
+
+
+export default Headers;
