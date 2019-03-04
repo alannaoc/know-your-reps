66a80d3f0578e086e6b56cfc5ce07f62babcb5f6
Alanna <oconnor.alannae@gmail.com>
2019-03-03 22:58:29 -0500

    updated form styling

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -7,8 +7,8 @@ class Form extends Component {
       <form action="submit" onSubmit={this.props.handleSubmit} >
         <fieldset>
           <label htmlFor="postCode">Enter a postal code to find local representatives:</label>
-          <input type="text" placeholder="(i.e A1A1A1)" onChange={this.props.handleChange} value={this.props.userPostalCode} required pattern="[A-Z][0-9][A-Z][0-9][A-Z][0-9]|[A-Z][0-9][A-Z] [0-9][A-Z][0-9]"/>
-          <button className="formSubmit" type="submit" onClick={this.props.handleClick}>Show me!</button>
+          <input className="headerInput" type="text" placeholder="(i.e A1A1A1)" onChange={this.props.handleChange} value={this.props.userPostalCode} required pattern="[A-Z][0-9][A-Z][0-9][A-Z][0-9]|[A-Z][0-9][A-Z] [0-9][A-Z][0-9]"/>
+          <button className="formSubmit headerInput" type="submit" onClick={this.props.handleClick}>Show me!</button>
         </fieldset>  
       </form>
     )
