7b01e2fbdcba7bcf6bc9f6d4c117ccc84f7b68b8
Alanna <oconnor.alannae@gmail.com>
2019-03-04 11:00:20 -0500

    Updates

---
+++ precache-manifest.349f08198e785083813a6d882a15a9d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c19034c1fd6489923e4",
+    "url": "/alannaOConnorProject5/static/css/main.f5a6bd08.chunk.css"
+  },
+  {
+    "revision": "6c19034c1fd6489923e4",
+    "url": "/alannaOConnorProject5/static/js/main.6c19034c.chunk.js"
+  },
+  {
+    "revision": "73793687119e5305523f",
+    "url": "/alannaOConnorProject5/static/js/runtime~main.73793687.js"
+  },
+  {
+    "revision": "a5aaa60d931d830db1cf",
+    "url": "/alannaOConnorProject5/static/js/2.a5aaa60d.chunk.js"
+  },
+  {
+    "revision": "cf121a217e523cc7110c5bdc5d4ebd86",
+    "url": "/alannaOConnorProject5/static/media/parliament.cf121a21.png"
+  },
+  {
+    "revision": "c5b0c0e4534899531c8c24247f164971",
+    "url": "/alannaOConnorProject5/index.html"
+  }
+];
